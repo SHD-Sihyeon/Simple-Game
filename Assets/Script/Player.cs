bcb1078cd9fdfc701ad14a1e43806f0209c54208
SH:D_Sihyeon <admin@shd.one>
2022-10-28 12:57:09 +0900

    edit Move

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,11 +8,14 @@ public class Player : MonoBehaviour
     [Range(0f, 10f)]
     private float JumpPower = 3f;
     [SerializeField]
+    private float movePower = 1f;
+    [SerializeField]
     private float maxJumpTime = 0.3f;
     private float jumpTime = 0.0f;
     private bool ifJumping = false;
     private Rigidbody2D rigidBody;
     private Animator animator;
+    private Vector3 movement;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -34,6 +37,10 @@ public class Player : MonoBehaviour
         }
         Debug.Log(ifJumping);
     }
+    void FixedUpdate()
+    {
+        Move();
+    }
     private void JumpStart()
     {
         jumpTime = 0.0f;
@@ -49,5 +56,18 @@ public class Player : MonoBehaviour
     private bool JumpKeyDown()
     {
         return Input.GetKeyDown(KeyCode.Space);
+    }
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+        }
+        transform.position += moveVelocity * movePower * Time.deltaTime;
     }
 }
